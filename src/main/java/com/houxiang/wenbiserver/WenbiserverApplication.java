65d3388a7b4a8596cf6be31dfc1f5ef885c958c1
houxiangrui <houxiangrui@baidu.com>
2018-12-22 19:52:32 +0800

    make the framework

---
+++ src/main/java/com/houxiang/wenbiserver/WenbiserverApplication.java
@@ -0,0 +1,14 @@
+package com.houxiang.wenbiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WenbiserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WenbiserverApplication.class, args);
+    }
+
+}
+
